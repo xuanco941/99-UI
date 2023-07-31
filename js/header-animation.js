78bc9bb33daed9204b169a93e92c9ea3a0761033
xuanco941 <xuanco941@gmail.com>
2023-07-31 11:55:53 +0700

    add event header

--- js/header-animation.js
+++ js/header-animation.js
@@ -1,9 +1,7 @@
 // DOMContentLoaded cây Dom sẵn sàng
 
-window.addEventListener('load', event => {
-
-    // Navbar shrink function
-
+window.addEventListener('DOMContentLoaded', event => {
+    //scroll qua 100 y thì gắn fixed và thay css màu, . . .
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
@@ -20,13 +18,11 @@ window.addEventListener('load', event => {
         }
 
     };
-
-    // Shrink the navbar 
     navbarShrink();
-
-    // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
+
+    //click showmenu mobile thì chuyển icon
     let btnShowMenu = document.querySelector('#btnShowMenu');
     btnShowMenu.addEventListener('click', () => {
         if (btnShowMenu.classList.contains('collapsed') == false) {
@@ -38,6 +34,45 @@ window.addEventListener('load', event => {
     });
 
 
+    //khi resize màn hình thay đổi size header bằng masthead, không phải trang home thì ẩn masthead
+    const ChangeMasthead = () => {
+        let masthead = document.querySelector('#masthead');
+        let header = document.querySelector('#header');
+        let nav_mobile = document.querySelector('#nav-mobile');
+        let main = document.querySelector('#main');
+
+        mastheadStyle = window.getComputedStyle(masthead).style;
+        if(masthead.classList.contains('.show')){
+            masthead.style.display = 'block';
+        }
+        else{
+            masthead.style.display = 'none';
+        }
+
+
+
+        navMobileDisplay = window.getComputedStyle(nav_mobile).display;
+        let headerHeight = masthead.clientHeight;
+        if(navMobileDisplay == 'none'){
+            header.style.height = headerHeight + "px";
+        }
+        else{
+            header.style.height = (headerHeight + 55.5) + "px";
+        }
+
+    }
+
+
+
+    ChangeMasthead();
+    window.addEventListener('resize', () => {
+        ChangeMasthead();
+    });
+})
+
+
+
+window.addEventListener('load', event => {
 
 });
 
